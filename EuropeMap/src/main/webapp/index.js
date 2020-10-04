80b5509652c7da81b2e991c42a58211ffd85cca7
Claes <claesvonh@gmail.com>
2020-10-04 15:39:45 +0200

    EU mp translations fixed

--- EuropeMap/src/main/webapp/index.js
+++ EuropeMap/src/main/webapp/index.js
@@ -1,6 +1,10 @@
 let url = 'http://restcountries.eu/rest/v1/alpha?codes=';
 
 const showCountry = async () => {
+                let countries = document.getElementsByTagName("path");
+            for (let state of countries) {
+                state.setAttribute('style', 'fill:#c0c0c0;stroke:#ffffff;stroke-width:0.40000001;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none');
+            }
     let country = event.target.id;
     event.target.setAttribute('style', 'fill: blue');
     let url2 = url + country;
@@ -11,9 +15,12 @@ const showCountry = async () => {
     const data = userData.map((country) => {
         var obj = country.translations;
         console.log(obj);
+        const obj2 = Object.values(obj);
+        //let result = country.translations.map(a => a.foo);
+
         return `Name: ${country.name} \n Population: ${country.population} \n Capital: ${country.capital} 
                         \n Alternative names: ${country.altSpellings} \n Borders: ${country.borders} 
-                        \n Translations: ` + JSON.stringify(obj);
+                        \n Translations: ` + obj2;
     });
     console.log(data);
     alert (data);
